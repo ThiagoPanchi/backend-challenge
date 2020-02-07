18f20506f50bafdf8208c4db82c1dfa0c57163de
Thiago Panchiniak <panchiniak@gmail.com>
2020-02-07 17:45:24 -0300

    include routes with consign

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+consign().include('routes').into(app);
+
 app.listen(3000, () =>{
   console.log('Servidor rodando!');
 });
